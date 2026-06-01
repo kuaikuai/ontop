14183b708b9824dd2e4a81265be4821cad8c3085
yuxiaofeng3 <yuxiaofeng3@hisense.com>
2026-06-01 10:04:46 +0800

    test: add SameSourceMerge two-node test (failing, Task 5 required)

--- core/optimization/src/test/java/it/unibz/inf/ontop/iq/optimizer/SameSourceMergeOptimizerTest.java
+++ core/optimization/src/test/java/it/unibz/inf/ontop/iq/optimizer/SameSourceMergeOptimizerTest.java
@@ -2,11 +2,14 @@ package it.unibz.inf.ontop.iq.optimizer;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import it.unibz.inf.ontop.dbschema.RelationDefinition;
 import it.unibz.inf.ontop.iq.IQ;
+import it.unibz.inf.ontop.iq.IQTree;
 import it.unibz.inf.ontop.iq.NaryIQTree;
 import it.unibz.inf.ontop.iq.UnaryIQTree;
 import it.unibz.inf.ontop.iq.node.*;
+import it.unibz.inf.ontop.iq.visit.impl.ExtensionalDataNodeExtractor;
 import it.unibz.inf.ontop.model.atom.DistinctVariableOnlyDataAtom;
 import org.junit.Test;
 
@@ -20,6 +23,40 @@ public class SameSourceMergeOptimizerTest {
     static {
         OfflineMetadataProviderBuilder3 builder = createMetadataProviderBuilder();
         T1_AR5 = builder.createRelationWithStringAttributes(1, 5, true);
+    }
+
+    @Test
+    public void testMergeTwoSameSourceNodes() {
+        ExtensionalDataNode dataNode1 = IQ_FACTORY.createExtensionalDataNode(
+                T1_AR5, ImmutableMap.of(0, A, 1, B));
+        ExtensionalDataNode dataNode2 = IQ_FACTORY.createExtensionalDataNode(
+                T1_AR5, ImmutableMap.of(0, C, 1, D));
+
+        ConstructionNode constr1 = IQ_FACTORY.createConstructionNode(ImmutableSet.of(A, B));
+        ConstructionNode constr2 = IQ_FACTORY.createConstructionNode(ImmutableSet.of(C, D));
+
+        NaryIQTree joinTree = IQ_FACTORY.createNaryIQTree(
+                IQ_FACTORY.createInnerJoinNode(),
+                ImmutableList.of(
+                        IQ_FACTORY.createUnaryIQTree(constr1, dataNode1),
+                        IQ_FACTORY.createUnaryIQTree(constr2, dataNode2)));
+
+        DistinctVariableOnlyDataAtom projectionAtom = ATOM_FACTORY.getDistinctVariableOnlyDataAtom(
+                ANS1_AR2_PREDICATE, A, C);
+
+        ConstructionNode topConstructionNode = IQ_FACTORY.createConstructionNode(projectionAtom.getVariables());
+        UnaryIQTree constructionTree = IQ_FACTORY.createUnaryIQTree(topConstructionNode, joinTree);
+
+        IQ initialQuery = IQ_FACTORY.createIQ(projectionAtom, constructionTree);
+
+        // After Task 5 implementation, merge should produce a single ExtensionalDataNode
+        // TODO: enable optimizer call after Task 5
+        int nodeCount = countExtensionalNodes(initialQuery.getTree());
+        assertEquals("Input should have 2 ExtensionalDataNodes", 2, nodeCount);
+    }
+
+    private static int countExtensionalNodes(IQTree tree) {
+        return (int) new ExtensionalDataNodeExtractor().transform(tree).count();
     }
 
 }
